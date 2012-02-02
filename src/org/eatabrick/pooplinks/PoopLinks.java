cab703367f73e2d81397c1474730431d5b076004
Alan Berndt <alan@eatabrick.org>
2012-02-01 18:16:16 -0700

    Initial commit

---
+++ src/org/eatabrick/pooplinks/PoopLinks.java
@@ -0,0 +1,15 @@
+package org.eatabrick.pooplinks;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PoopLinks extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
